31a6f5ccf13ea0959f328b16ace66fed7ae19df6
Quynh <taq2105@gmail.com>
2024-07-08 12:23:08 +0700

    update pkg

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -5,7 +5,6 @@ import Loader from '@/components/Loader';
 import { useUser } from '@clerk/nextjs';
 import { StreamVideoClient, StreamVideo } from '@stream-io/video-react-sdk';
 import { ReactNode, useEffect, useState } from 'react';
-import { StreamChat } from 'stream-chat';
   
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
